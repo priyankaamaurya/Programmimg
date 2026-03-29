80513b4c9992f128cb065f35b6c69fbe5061c026
priyanka <prachii8826@gmail.com>
2026-03-29 21:33:16 +0530

    added files

--- Day24/src/com/string/DuplicateChar.java
+++ Day24/src/com/string/DuplicateChar.java
@@ -1,5 +1,24 @@
 package com.string;
 
-public class DuplicateChar {
+import java.util.HashSet;
 
+public class DuplicateChar {
+	
+	public static void main(String[] args) {
+
+	        String str = "programming";
+	        HashSet<Character> set = new HashSet<>();
+
+	        for (char ch : str.toCharArray()) {
+	            if (set.contains(ch)) {
+	                System.out.println("First duplicate character: " + ch);
+	                return;
+	            } else {
+	                set.add(ch);
+	            }
+	        }
+
+	        System.out.println("No duplicate character found");
+	 }
+	
 }
--- Day24/src/com/string/NonRepeating.java
+++ Day24/src/com/string/NonRepeating.java
@@ -1,5 +1,26 @@
 package com.string;
 
 public class NonRepeating {
+	
+	    public static void main(String[] args) {
 
+	        String str = "swiss";
+	        int[] freq = new int[26]; // for lowercase letters
+
+	        // Count frequency
+	        for (char ch : str.toCharArray()) {
+	            freq[ch - 'a']++;
+	        }
+
+	        // Find first non-repeating
+	        for (char ch : str.toCharArray()) {
+	            if (freq[ch - 'a'] == 1) {
+	                System.out.println("First non-repeating character: " + ch);
+	                return;
+	            }
+	        }
+
+	        System.out.println("No non-repeating character found");
+	    }
+	
 }
