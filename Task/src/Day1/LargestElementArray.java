909b7d9125ac5ac21b7e31fd289caffc22516051
priyanka <prachii8826@gmail.com>
2026-06-26 22:16:27 +0530

    added files

--- Task/src/Day1/LargestElementArray.java
+++ Task/src/Day1/LargestElementArray.java
@@ -1,8 +1,10 @@
 package Day1;
 
 public class LargestElementArray {
+	
 	public static int findMax(int[] arr) {
-        int max = arr[0]; // assume first element is max
+		
+        int max = arr[0];                      // assume first element is max
         
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
