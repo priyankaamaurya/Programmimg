83b580261a22dffdb51ddac4f92d875a85c5ab5d
priyanka <prachii8826@gmail.com>
2026-06-29 23:07:23 +0530

    added files

---
+++ Task/src/Day3/ReverseArray.java
@@ -0,0 +1,27 @@
+package Day3;
+
+public class ReverseArray {
+    public static void reverse(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            // swap
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverse(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
