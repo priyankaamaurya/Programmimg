3e8e4ba4820c8915c423803a44a9f90635969ed1
priyanka <prachii8826@gmail.com>
2026-07-03 22:59:23 +0530

    added files

---
+++ Task/src/Day7/TwoSum.java
@@ -0,0 +1,35 @@
+package Day7;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+	public static int[] twoSum(int[] arr, int target) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+        // key = number, value = index
+
+		for (int i = 0; i < arr.length; i++) {
+			int complement = target - arr[i];
+			
+            // check if complement exists
+			if (map.containsKey(complement)) {
+				return new int[] {map.get(complement), i}; 
+			}
+			
+            // store current element
+			map.put(arr[i], i);
+		}
+		
+		// if no solution found
+		throw new IllegalArgumentException("No two sum solution");
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] arr = {3,2,0,7,8,4,5,3,0};
+		int target = 9;
+		
+		int[] result = twoSum(arr, target);
+		System.out.println(result[0] + " " + result[1]);
+	}
+}
