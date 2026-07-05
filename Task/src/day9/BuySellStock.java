a461944609515a475ed63b14e8551857bec237b3
priyanka <prachii8826@gmail.com>
2026-07-05 22:15:32 +0530

    added files

---
+++ Task/src/day9/BuySellStock.java
@@ -0,0 +1,34 @@
+package day9;
+
+public class BuySellStock {
+	
+	public static int maxProfit(int[] prices) {
+		int minPrice = Integer.MAX_VALUE;
+		int maxProfit = 0;
+		
+		for(int price : prices) {
+			
+			// update minimum price
+			if (price < minPrice) {
+				minPrice = price;
+			}
+			
+			// calculate profit
+			int profit = price - minPrice;
+			
+            // update maximum profit
+			if(profit > maxProfit) {
+				maxProfit = profit; 
+			}
+		}
+		
+		return maxProfit;
+	} 
+	
+	public static void main(String[] args) {
+		
+		int[] prices = {7, 1, 5, 3, 6, 4};
+		System.out.println("Max profits: " + maxProfit(prices));
+	}
+
+}
