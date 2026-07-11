a3b919470901ee2066f78bdfe9224a7203459eb9
priyanka <prachii8826@gmail.com>
2026-07-11 23:44:30 +0530

    added files

---
+++ Task/src/day12/Palindrome.java
@@ -0,0 +1,23 @@
+package day12;
+
+public class Palindrome {
+	
+	public static boolean isPalindrome(String s) {
+		int left = 0, right = s.length() - 1;
+		
+		while(left < right) {
+			if (s.charAt(left) != s.charAt(right)) {
+				return false;
+			}
+			
+			left++;
+			right--;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(isPalindrome("madam"));
+	}
+
+}
