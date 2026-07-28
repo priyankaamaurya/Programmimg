10e19ad6cb22560ff7a7f489dfefaa49cc5a71dc
priyanka <prachii8826@gmail.com>
2026-07-28 23:30:32 +0530

    added files

---
+++ Task/src/day20/ReverseString.java
@@ -0,0 +1,29 @@
+package day20;
+
+public class ReverseString {
+
+	public static String reverseString(String str) {
+		
+		char[] arr = str.toCharArray();
+		
+		int left = 0;
+		int right = arr.length - 1;
+		
+		while (left < right) {
+			char temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			
+			left++;
+			right--;
+		}		
+		return new String(arr);		
+	}
+	
+	public static void main(String[] args) {
+		
+		String str = "Qspiders";
+		
+		System.out.println("Reverse String is: " + reverseString(str));
+	}
+}
