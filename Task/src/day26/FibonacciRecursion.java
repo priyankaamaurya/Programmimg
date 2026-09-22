edf11b10cede8a5fe09acf101f99cb7aa1a07c7f
priyanka <prachii8826@gmail.com>
2026-09-22 20:55:42 +0530

    added files

--- Task/src/day26/FibonacciRecursion.java
+++ Task/src/day26/FibonacciRecursion.java
@@ -12,14 +12,10 @@ public class FibonacciRecursion {
 		return fibb(n-1) + fibb(n-2);
 	}
 	
-	public static void main(String[] args) {
-		
-		int n = 7;
-		
+	public static void main(String[] args) {	
+		int n = 7;	
 		for(int i = 0; i < n; i++) {
-			
 			System.out.println(fibb(i) + " ");
-			
 		}
 	}
 }
