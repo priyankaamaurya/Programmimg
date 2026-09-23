bc134cf839b090972a7f51fb6046dc9713f4d3fa
priyanka <prachii8826@gmail.com>
2026-09-23 21:16:11 +0530

    added files

--- Task/src/day25/Fibonacci.java
+++ Task/src/day25/Fibonacci.java
@@ -7,16 +7,16 @@ public class Fibonacci {
 		int a = 0;
 		int b = 1;
 		
-		// Step 1: Print first two numbers
+		// Step 1- Print first two numbers
 		System.out.print(a + " " + b + " ");
 		
-		// Step 2: Generate remaining numbers
+		// Step 2- Generate remaining numbers
 		 for (int i = 2; i < n; i++) {
 			 
 			 int next = a + b;             // sum of previous two
 	         System.out.print(next + " ");
 	         
-	         // Step 3: Update values
+	         // Step 3- Update values
 	         a = b;
 	         b = next;
 	         
